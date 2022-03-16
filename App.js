98ef324cc48feda1f2ff663fcd8b098a8177a3b5
nabendu82 <nabendu.biswas@gmail.com>
2022-03-16 15:11:48 +0530

    Tutorial 2 complete

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
-import { StyleSheet, Text, View, Button, Linking } from 'react-native'
 import React from 'react'
+import { StyleSheet, View, SafeAreaView, Text, SectionList } from 'react-native'
 
 const App = () => {
+  const DATA = [
+    { title: 'Learn React Native', data: ['Item 1', 'Item 2', 'Item 3']},
+    { title: 'Learn PostgreSQL', data: ['Item 1', 'Item 2', 'Item 3'] },
+    { title: 'Learn Swift', data: ['Item 1'] },
+    { title: 'Learn Flutter', data: ['Item 1', 'Item 2'] },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>TWD</Text>
-      <Button 
-        title='TWD YT' 
-        onPress={()=>{Linking.openURL('https://www.youtube.com/channel/UCRf7mazcufqZxd2pR3oQJuQ')}} 
-      />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <SectionList
+          keyExtractor={index => index.toString()}
+          sections={DATA}
+          renderItem={({ item }) => (
+            <Text style={styles.text}>{item}</Text>
+          )}
+          renderSectionHeader={({ section }) => (
+            <View style={styles.item}>
+              <Text style={styles.text}>{section.title}</Text>
+            </View>
+          )}
+        />
+      </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  titleText: {
-    fontSize: 20,
-    margin: 10,
-  },
+  container: { flex: 1, backgroundColor: 'lightblue'},
+  item: { margin: 10, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightyellow' },
+  text: { fontSize: 40, color: 'black', padding: 10 },
 })
 
 export default App
